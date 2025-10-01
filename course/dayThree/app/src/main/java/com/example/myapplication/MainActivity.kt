b1d309d536152c73b9a141970eac8642103d1ce7
Abdullah Al Zayed <rudro54@gmail.com>
2025-10-01 13:51:09 -0500

    day three  assignment completed

--- course/dayThree/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ course/dayThree/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -13,35 +13,33 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
+
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun MyScreen(modifier: Modifier= Modifier){
+    Scaffold { innerPadding->
+        Text(
+            text = "MIU",
+            modifier= Modifier.padding(innerPadding)
+
+        )
+        Text(
+            text = "Fairfield",
+            modifier= Modifier.padding(innerPadding)
+
+
+        )
+
+
+    }
+
+}
+
+@Preview(
+    showSystemUi=true
     )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
-    }
+fun MyScreenPreview(){
+    MyScreen()
 }

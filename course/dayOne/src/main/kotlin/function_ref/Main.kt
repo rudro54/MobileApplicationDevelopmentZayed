30f0758df0c060a90ac768dc2caf663290f38f30
Abdullah Al Zayed <rudro54@gmail.com>
2025-10-02 00:58:21 -0500

    day three  assignment completed

--- course/dayOne/src/main/kotlin/function_ref/Main.kt
+++ course/dayOne/src/main/kotlin/function_ref/Main.kt
@@ -1,6 +1,7 @@
 package edu.miu.function_ref
-
+//fun add(number1:Int, number2:Int)=number1+number2
 fun main() {
+    //the function add() is stored in a variable call addFunction .
     var fn = ::add // function reference operator
     var sub = {a: Int, b: Int ->
         a - b
